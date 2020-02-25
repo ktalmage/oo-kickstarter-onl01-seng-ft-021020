8d64ce472d941998d88ccb15bdad010fec38d51b
Kyle Talmage <ktalmage85@gmail.com>
2020-02-25 22:16:49 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,10 +3,21 @@ class Project
   attr_accessor 
   attr_reader :title, :backers
   
+  @@all = []
+  
     def initialize(title)
       @title = title
       @backers = []
+      save
     end 
+    
+    def self.all
+      @@all
+    end
+    
+    def save
+      @@all << self
+    end
     
     def self.add_backer(backer)
       @backer << Backer
